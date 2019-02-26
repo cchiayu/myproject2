16e84a733d16667d6ea911e37ef67eb656f67880
佳妤 <@LAPTOP-1OJTGCDU>
2019-02-26 15:36:18 +0800

    2/26 hw

---
+++ myproject/src/classExercise/Kids.java
@@ -0,0 +1,15 @@
+package classExercise;
+
+import classExercise.Parents;
+
+public class Kids extends Parents{
+
+	public Kids(String name, float weight, float height) {
+		super(name, weight, height);
+	
+	}
+	
+		
+
+
+}
