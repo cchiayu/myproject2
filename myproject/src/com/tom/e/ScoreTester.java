2d8ca64873495d3fa8564a405edbd4e1dd0f2f1c
cchiayu <love.yy0114@gmail.com>
2019-04-30 00:11:13 +0800

    0429改正

--- myproject/src/com/tom/e/ScoreTester.java
+++ myproject/src/com/tom/e/ScoreTester.java
@@ -1,44 +1,34 @@
 package com.tom.e;
 	import java.util.Scanner;
 	public class ScoreTester {
-	public static void main(String[] args) throws MyException {
+	public static void main(String[] args) {
 
 	 Scanner scanner = new Scanner(System.in);
 	    try {
 	    System.out.print("請輸入英文分數");
-	    int h= Integer.parseInt(scanner.nextLine());
+	    String english = scanner.nextLine();
+	    int h= Integer.parseInt(english);
+	    if(h >100 || h < 0){
+	    	h= (Integer) null;
+	    }
 	    System.out.print("請輸入數學分數");
-	    int m = Integer.parseInt(scanner.nextLine());
-	  //  Score e = new Score(scanner.nextLine());
-	  //  Score m = new Score(scanner.nextLine());
+	    String math = scanner.nextLine();
+	    int m = Integer.parseInt(math);
+	    if(m>100 || m< 0){
+	    	m= (Integer) null;
+	    }
 	    int average= (h + m)/2;
 		System.out.print("平均為"+ average);
 	     }catch(NumberFormatException e) {
 	      System.out.println("訊息格式錯誤");
-	   } 
-	    
-	    	int english;
-	    	int math  ;
-
-	    	public ScoreTester (int math,int english) {
-	    	this.math = math;
-	    	this.english = english; 	
-	    	
-		if(math >100 || math < 0) {
-		   System.out.println("訊息成績應為0~100");
-			}else{
-			//this.math = math;
-			}
-		if(english >100 || english < 0){
-			System.out.println("訊息成績應為0~100");
-			}else{
-				 
-			//this.english = english;
-			}
+	     }catch(NullPointerException e) {
+	         System.out.println("訊息成績應為0~100");
 	     }
-	    }
-	
 	}
-			
+	}
+	      
+	    //NullPointerException  當所需資料為null時拋出異常
+		//NumberFormatException 資料格式錯誤	
+		//ArithmeticException   用0做為除數使得結果產生異常
+		//h=h/0
 	
-
