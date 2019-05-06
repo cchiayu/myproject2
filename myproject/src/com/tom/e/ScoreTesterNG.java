82f6422c088fb2d9808de9d45bb3c39710da4c99
cchiayu <love.yy0114@gmail.com>
2019-05-06 23:33:18 +0800

    0506 pizza ng

--- myproject/src/com/tom/e/ScoreTesterNG.java
+++ myproject/src/com/tom/e/ScoreTesterNG.java
@@ -20,15 +20,14 @@ package com.tom.e;
 	    	int average= (h + m)/2;
 	    	System.out.print("平均為"+ average);
 	    	} 	catch(NumberFormatException e) {
-	    	// e.printStackTrace();用於觀察錯誤
 	    	System.out.println("訊息格式錯誤");
 	    	}	catch(NullPointerException e)  {
-	    	// e.printStackTrace();用於觀察錯誤
-	        System.out.println("訊息成績應為0~100");
+	    	System.out.println("訊息成績應為0~100");
 	    	}
 			}
 }  
 		/*NullPointerException  當所需資料為null時拋出異常
 	  	NumberFormatException   資料格式錯誤	
-	  	ArithmeticException     用0做為除數使得結果產生異常,h=h/0*/
+	  	ArithmeticException     用0做為除數使得結果產生異常,h=h/0
+	  	e.printStackTrace();用於觀察錯誤*/
 	
---
+++ myproject/src/com/tom/f/Pizza.java
@@ -0,0 +1,35 @@
+package com.tom.f;
+
+import java.util.Scanner;
+
+public class Pizza {
+
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+		 try {
+		 System.out.print("Pizza想切幾塊?");
+		 String piece = scanner.nextLine();
+	     int n = Integer.parseInt(piece);
+	    	if (n == 0) {
+	    		System.out.println("你確定不要切?");
+	    	}else if(n>=1 && n <=7) {
+	    		System.out.println("數量錯誤");
+	    	}else if(n>=9 && n <=11) {
+	    		System.out.println("數量錯誤");
+	    	}else if(n>=13 && n <=15) {
+	    		System.out.println("數量錯誤");
+	    	}else if (n<=0) {
+	    		System.out.println("超出範圍");
+	    	}else if (n>=17) {
+		    		System.out.println("超出範圍");
+	    	}else if (n==8) {
+			    		System.out.println("謝謝選購");
+	    	}else if (n==12) {
+	    		System.out.println("謝謝選購");
+	    	}else if (n==16) {
+	    		System.out.println("謝謝選購");
+	    	}} catch(NumberFormatException e) {
+		    	System.out.println("格式錯誤");
+		    	}
+}
+	}
