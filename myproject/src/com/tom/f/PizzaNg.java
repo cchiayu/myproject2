5e813c1273c3969270cdbe5bf7a283e4fa33ae4d
cchiayu <love.yy0114@gmail.com>
2019-05-06 23:59:24 +0800

    0506 pizza ng

--- myproject/src/com/tom/f/PizzaNg.java
+++ myproject/src/com/tom/f/PizzaNg.java
@@ -14,13 +14,12 @@ public class PizzaNg {
 	    		System.out.println("你確定不要切?");
 	    	}else if((n>=1 && n <=7)||(n>=9 && n <=11)||(n>=13 && n <=15)){
 	    		System.out.println("數量錯誤");
-	    	}else if (n<=0) {
+	    	}else if ((n<=0)||(n>=17)) {
 	    		System.out.println("超出範圍");
-	    	}else if (n>=17) {
-		    		System.out.println("超出範圍");
 	    	}else if ((n==8)||(n==12)||(n==16)) {
-			    		System.out.println("謝謝選購");
-	    	}} catch(NumberFormatException e) {
+			   System.out.println("謝謝選購");
+	  }
+	    	} catch(NumberFormatException e) {
 		    	System.out.println("格式錯誤");
 		    	}
 }
