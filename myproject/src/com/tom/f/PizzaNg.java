dff010329c07ae04ec5e9c4af30b44ac6ba7ef0d
cchiayu <love.yy0114@gmail.com>
2019-05-06 23:56:26 +0800

    0506 pizza ng

--- myproject/src/com/tom/f/PizzaNg.java
+++ myproject/src/com/tom/f/PizzaNg.java
@@ -12,22 +12,14 @@ public class PizzaNg {
 	     int n = Integer.parseInt(piece);
 	    	if (n == 0) {
 	    		System.out.println("你確定不要切?");
-	    	}else if(n>=1 && n <=7) {
-	    		System.out.println("數量錯誤");
-	    	}else if(n>=9 && n <=11) {
-	    		System.out.println("數量錯誤");
-	    	}else if(n>=13 && n <=15) {
+	    	}else if((n>=1 && n <=7)||(n>=9 && n <=11)||(n>=13 && n <=15)){
 	    		System.out.println("數量錯誤");
 	    	}else if (n<=0) {
 	    		System.out.println("超出範圍");
 	    	}else if (n>=17) {
 		    		System.out.println("超出範圍");
-	    	}else if (n==8) {
+	    	}else if ((n==8)||(n==12)||(n==16)) {
 			    		System.out.println("謝謝選購");
-	    	}else if (n==12) {
-	    		System.out.println("謝謝選購");
-	    	}else if (n==16) {
-	    		System.out.println("謝謝選購");
 	    	}} catch(NumberFormatException e) {
 		    	System.out.println("格式錯誤");
 		    	}
